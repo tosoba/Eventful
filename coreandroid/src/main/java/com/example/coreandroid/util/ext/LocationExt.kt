b7e0fb91798ac45923bd0a56d8f43c925a6cbd3d
Tomasz Osoba <tomasz.osoba@comarch.pl>
2019-09-25 15:33:03 +0200

    RxLocation current

--- coreandroid/src/main/java/com/example/coreandroid/util/ext/LocationExt.kt
+++ coreandroid/src/main/java/com/example/coreandroid/util/ext/LocationExt.kt
@@ -2,10 +2,12 @@ package com.example.coreandroid.util.ext
 
 import android.location.Address
 import android.location.Location
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.maps.model.LatLng
 import com.patloew.rxlocation.RxLocation
 import io.nlopez.smartlocation.SmartLocation
 import kotlinx.coroutines.rx2.await
+import java.util.concurrent.TimeUnit
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
@@ -20,3 +22,12 @@ suspend fun RxLocation.reverseGeocode(location: Location): Address? = geocoding(
     .fromLocation(location)
     .onErrorComplete()
     .await()
+
+suspend fun RxLocation.currentLocation(): Location? = location()
+    .lastLocation()
+    .switchIfEmpty(
+        location().updates(
+            LocationRequest.create().setNumUpdates(1)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        ).timeout(15, TimeUnit.SECONDS).firstElement().onErrorComplete()
+    ).await()
