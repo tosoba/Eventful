fc1aa34fe0b1f72329d085c168e51d019e94a292
Tomasz Osoba <tomasz.osoba@comarch.pl>
2020-04-23 16:57:15 +0200

    Action mode fixes.

--- coreandroid/src/main/java/com/example/coreandroid/controller/EventsSelectionActionModeController.kt
+++ coreandroid/src/main/java/com/example/coreandroid/controller/EventsSelectionActionModeController.kt
@@ -3,7 +3,6 @@ package com.example.coreandroid.controller
 import android.view.ActionMode
 import android.view.Menu
 import android.view.MenuItem
-import androidx.annotation.MenuRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
@@ -12,7 +11,7 @@ import com.example.coreandroid.util.ext.plusAssign
 
 interface EventsSelectionActionModeController {
     fun update(numberOfSelectedEvents: Int)
-    fun finish(clearSelection: Boolean)
+    fun finish(destroy: Boolean)
 }
 
 fun Fragment.eventsSelectionActionModeController(
@@ -20,12 +19,32 @@ fun Fragment.eventsSelectionActionModeController(
     itemClickedCallbacks: Map<Int, () -> Unit>,
     onDestroyActionMode: () -> Unit
 ): EventsSelectionActionModeController {
+    var destroyOnFinish = true
     var actionMode: ActionMode? = null
-    val callback = ToolbarActionModeCallback(
-        menuId,
-        itemClickedCallbacks,
-        onDestroyActionMode
-    )
+    val callback = object : ActionMode.Callback {
+        override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
+            itemClickedCallbacks[item.itemId]?.invoke()
+            return false
+        }
+
+        override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
+            mode.menuInflater.inflate(menuId, menu)
+            return true
+        }
+
+        override fun onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean {
+            itemClickedCallbacks.keys.forEach {
+                menu.findItem(it)?.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+            }
+            return true
+        }
+
+        override fun onDestroyActionMode(mode: ActionMode?) {
+            if (destroyOnFinish) onDestroyActionMode()
+            actionMode = null
+            destroyOnFinish = true
+        }
+    }
     val controller = object : EventsSelectionActionModeController {
         override fun update(numberOfSelectedEvents: Int) {
             if (actionMode == null && numberOfSelectedEvents > 0) {
@@ -39,8 +58,8 @@ fun Fragment.eventsSelectionActionModeController(
             }
         }
 
-        override fun finish(clearSelection: Boolean) {
-            callback.callOnDestroy = clearSelection
+        override fun finish(destroy: Boolean) {
+            destroyOnFinish = destroy
             actionMode?.finish()
             actionMode = null
         }
@@ -57,33 +76,3 @@ fun Fragment.eventsSelectionActionModeController(
     return controller
 }
 
-private class ToolbarActionModeCallback(
-    @MenuRes private val menuId: Int,
-    private val itemClickedCallbacks: Map<Int, () -> Unit>,
-    private val onDestroyActionMode: () -> Unit
-) : ActionMode.Callback {
-
-    var callOnDestroy = true
-
-    override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
-        itemClickedCallbacks[item.itemId]?.invoke()
-        return false
-    }
-
-    override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
-        mode.menuInflater.inflate(menuId, menu)
-        return true
-    }
-
-    override fun onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean {
-        itemClickedCallbacks.keys.forEach {
-            menu.findItem(it)?.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
-        }
-        return true
-    }
-
-    override fun onDestroyActionMode(mode: ActionMode?) {
-        if (callOnDestroy) onDestroyActionMode()
-        callOnDestroy = true
-    }
-}
