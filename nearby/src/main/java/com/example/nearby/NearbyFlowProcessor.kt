6684d8a7be2936b927f87a255a08189ba35eaedf
Tomasz Osoba <tomasz.osoba@comarch.pl>
2020-06-12 10:48:38 +0200

    NearbyFlowProcessor.

---
+++ nearby/src/main/java/com/example/nearby/NearbyFlowProcessor.kt
@@ -0,0 +1,130 @@
+package com.example.nearby
+
+import androidx.lifecycle.SavedStateHandle
+import com.example.core.model.app.LatLng
+import com.example.core.model.app.LocationState
+import com.example.core.model.app.LocationStatus
+import com.example.core.provider.ConnectedStateProvider
+import com.example.core.provider.LocationStateProvider
+import com.example.core.usecase.GetNearbyEvents
+import com.example.core.usecase.GetPagedEventsFlow
+import com.example.core.usecase.SaveEvents
+import com.example.core.util.Loading
+import com.example.core.util.ext.flatMapFirst
+import com.example.coreandroid.base.FlowProcessor
+import com.example.coreandroid.util.addedToFavouritesMessage
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import javax.inject.Inject
+
+@FlowPreview
+@ExperimentalCoroutinesApi
+class NearbyFlowProcessor @Inject constructor(
+    private val getNearbyEvents: GetNearbyEvents,
+    private val saveEvents: SaveEvents,
+    private val getPagedEventsFlow: GetPagedEventsFlow,
+    private val connectedStateProvider: ConnectedStateProvider,
+    private val locationStateProvider: LocationStateProvider,
+    private val ioDispatcher: CoroutineDispatcher
+) : FlowProcessor<NearbyIntent, NearbyStateUpdate, NearbyState, NearbySignal> {
+
+    override fun updates(
+        coroutineScope: CoroutineScope,
+        intents: Flow<NearbyIntent>,
+        currentState: () -> NearbyState,
+        states: StateFlow<NearbyState>,
+        intent: suspend (NearbyIntent) -> Unit,
+        signal: suspend (NearbySignal) -> Unit,
+        savedStateHandle: SavedStateHandle
+    ): Flow<NearbyStateUpdate> = merge(
+        intents.updates(coroutineScope, currentState, intent, signal),
+        connectedStateProvider.updates(currentState),
+        connectedStateProvider.snackbarUpdates(currentState),
+        locationStateProvider.updates(currentState),
+        locationStateProvider.snackbarUpdates
+    )
+
+    private fun Flow<NearbyIntent>.updates(
+        coroutineScope: CoroutineScope,
+        currentState: () -> NearbyState,
+        intent: suspend (NearbyIntent) -> Unit,
+        signal: suspend (NearbySignal) -> Unit
+    ): Flow<NearbyStateUpdate> = merge(
+        filterIsInstance<NearbyIntent.ClearSelectionClicked>()
+            .map { NearbyStateUpdate.ClearSelection },
+        filterIsInstance<NearbyIntent.EventLongClicked>()
+            .map { NearbyStateUpdate.ToggleEventSelection(it.event) },
+        filterIsInstance<NearbyIntent.HideSnackbar>()
+            .map { NearbyStateUpdate.HideSnackbar },
+        filterIsInstance<NearbyIntent.LoadMoreResults>()
+            .loadMoreResultsUpdates(currentState),
+        filterIsInstance<NearbyIntent.AddToFavouritesClicked>()
+            .addToFavouritesUpdates(coroutineScope, currentState, intent, signal)
+    )
+
+    private fun ConnectedStateProvider.updates(
+        currentState: () -> NearbyState
+    ): Flow<NearbyStateUpdate> = connectedStates.filter { connected ->
+        currentState().run { connected && events.loadingFailed && events.data.isEmpty() }
+    }.flatMapFirst {
+        locationStateProvider.locationStates.notNullLatLng.take(1)
+    }.flatMapLatest { latLng -> loadingEventsUpdates(latLng, currentState) }
+
+    private fun ConnectedStateProvider.snackbarUpdates(
+        currentState: () -> NearbyState
+    ): Flow<NearbyStateUpdate> = connectedStates.filter { connected ->
+        currentState().run { !connected && events.loadingFailed }
+    }.flatMapFirst {
+        locationStateProvider.locationStates.notNullLatLng.take(1)
+    }.map { NearbyStateUpdate.NoConnectionSnackbar }
+
+    private fun LocationStateProvider.updates(
+        currentState: () -> NearbyState
+    ): Flow<NearbyStateUpdate> = locationStates.notNullLatLng
+        .filter { currentState().events.data.isEmpty() } // TODO: this won't work with refreshing with SwipeRefreshLayout
+        .flatMapLatest { latLng -> loadingEventsUpdates(latLng, currentState) }
+
+    private val LocationStateProvider.snackbarUpdates: Flow<NearbyStateUpdate>
+        get() = locationStates.filter { it.latLng == null && it.status !is LocationStatus.Initial }
+            .map { (_, status) ->
+                NearbyStateUpdate.LocationSnackbar(status, locationStateProvider::reloadLocation)
+            }
+
+    private fun Flow<NearbyIntent.LoadMoreResults>.loadMoreResultsUpdates(
+        currentState: () -> NearbyState
+    ): Flow<NearbyStateUpdate> = filterNot {
+        val events = currentState().events
+        events.status is Loading || !events.canLoadMore || events.data.isEmpty()
+    }.flatMapFirst {
+        locationStateProvider.locationStates.notNullLatLng.take(1)
+    }.flatMapFirst { latLng -> loadingEventsUpdates(latLng, currentState) }
+
+    private val Flow<LocationState>.notNullLatLng get() = map { it.latLng }.filterNotNull()
+
+    private fun loadingEventsUpdates(
+        latLng: LatLng,
+        currentState: () -> NearbyState
+    ): Flow<NearbyStateUpdate> = getPagedEventsFlow(
+        currentEvents = currentState().events,
+        toEvent = { selectable -> selectable.item }
+    ) { offset ->
+        getNearbyEvents(latLng.lat, latLng.lng, offset)
+    }.map { resource ->
+        NearbyStateUpdate.Events.Loaded(resource)
+    }.onStart<NearbyStateUpdate> { emit(NearbyStateUpdate.Events.Loading) }
+
+    private fun Flow<NearbyIntent.AddToFavouritesClicked>.addToFavouritesUpdates(
+        coroutineScope: CoroutineScope,
+        currentState: () -> NearbyState,
+        intent: suspend (NearbyIntent) -> Unit,
+        signal: suspend (NearbySignal) -> Unit
+    ): Flow<NearbyStateUpdate> = map {
+        val selectedEvents = currentState().events.data.filter { it.selected }.map { it.item }
+        withContext(ioDispatcher) { saveEvents(selectedEvents) }
+        signal(NearbySignal.FavouritesSaved)
+        NearbyStateUpdate.Events.AddedToFavourites(
+            snackbarText = addedToFavouritesMessage(eventsCount = selectedEvents.size),
+            onSnackbarDismissed = { coroutineScope.launch { intent(NearbyIntent.HideSnackbar) } }
+        )
+    }
+}
--- nearby/src/main/java/com/example/nearby/NearbyViewModel.kt
+++ nearby/src/main/java/com/example/nearby/NearbyViewModel.kt
@@ -1,20 +1,8 @@
 package com.example.nearby
 
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.viewModelScope
-import com.example.core.model.app.LatLng
-import com.example.core.model.app.LocationState
-import com.example.core.model.app.LocationStatus
-import com.example.core.provider.ConnectedStateProvider
-import com.example.core.provider.LocationStateProvider
-import com.example.core.usecase.GetNearbyEvents
-import com.example.core.usecase.GetPagedEventsFlow
-import com.example.core.usecase.SaveEvents
-import com.example.core.util.Loading
-import com.example.core.util.ext.flatMapFirst
-import com.example.coreandroid.base.BaseViewModel
+import com.example.coreandroid.base.FlowViewModel
 import com.example.coreandroid.di.viewmodel.AssistedSavedStateViewModelFactory
-import com.example.coreandroid.util.addedToFavouritesMessage
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
 import kotlinx.coroutines.*
@@ -23,99 +11,15 @@ import kotlinx.coroutines.flow.*
 @ExperimentalCoroutinesApi
 @FlowPreview
 class NearbyViewModel @AssistedInject constructor(
-    private val getNearbyEvents: GetNearbyEvents,
-    private val saveEvents: SaveEvents,
-    private val getPagedEventsFlow: GetPagedEventsFlow,
-    private val connectedStateProvider: ConnectedStateProvider,
-    private val locationStateProvider: LocationStateProvider,
-    private val ioDispatcher: CoroutineDispatcher,
-    @Assisted private val savedStateHandle: SavedStateHandle
-) : BaseViewModel<NearbyIntent, NearbyStateUpdate, NearbyState, NearbySignal>(
-    savedStateHandle["initialState"] ?: NearbyState()
+    processor: NearbyFlowProcessor,
+    @Assisted savedStateHandle: SavedStateHandle
+) : FlowViewModel<NearbyIntent, NearbyStateUpdate, NearbyState, NearbySignal>(
+    initialState = savedStateHandle["initialState"] ?: NearbyState(),
+    processor = processor,
+    savedStateHandle = savedStateHandle
 ) {
-
     @AssistedInject.Factory
     interface Factory : AssistedSavedStateViewModelFactory<NearbyViewModel> {
         override fun create(savedStateHandle: SavedStateHandle): NearbyViewModel
     }
-
-    init {
-        start()
-    }
-
-    override val updates: Flow<NearbyStateUpdate>
-        get() = merge(
-            intents.updates,
-            connectedStateProvider.updates,
-            connectedStateProvider.snackbarUpdates,
-            locationStateProvider.updates,
-            locationStateProvider.snackbarUpdates
-        )
-
-    private val Flow<NearbyIntent>.updates: Flow<NearbyStateUpdate>
-        get() = merge(
-            filterIsInstance<NearbyIntent.ClearSelectionClicked>()
-                .map { NearbyStateUpdate.ClearSelection },
-            filterIsInstance<NearbyIntent.EventLongClicked>()
-                .map { NearbyStateUpdate.ToggleEventSelection(it.event) },
-            filterIsInstance<NearbyIntent.HideSnackbar>()
-                .map { NearbyStateUpdate.HideSnackbar },
-            filterIsInstance<NearbyIntent.LoadMoreResults>().loadMoreResultsUpdates,
-            filterIsInstance<NearbyIntent.AddToFavouritesClicked>().addToFavouritesUpdates
-        )
-
-    private val ConnectedStateProvider.updates: Flow<NearbyStateUpdate>
-        get() = connectedStates.filter { connected ->
-            state.run { connected && events.loadingFailed && events.data.isEmpty() }
-        }.flatMapFirst {
-            locationStateProvider.locationStates.notNullLatLng.take(1)
-        }.flatMapLatest { latLng -> loadingEventsUpdates(latLng) }
-
-    private val ConnectedStateProvider.snackbarUpdates: Flow<NearbyStateUpdate>
-        get() = connectedStates.filter { connected ->
-            state.run { !connected && events.loadingFailed }
-        }.flatMapFirst {
-            locationStateProvider.locationStates.notNullLatLng.take(1)
-        }.map { NearbyStateUpdate.NoConnectionSnackbar }
-
-    private val LocationStateProvider.snackbarUpdates: Flow<NearbyStateUpdate>
-        get() = locationStates.filter { it.latLng == null && it.status !is LocationStatus.Initial }
-            .map { (_, status) ->
-                NearbyStateUpdate.LocationSnackbar(status, locationStateProvider::reloadLocation)
-            }
-
-    private val LocationStateProvider.updates: Flow<NearbyStateUpdate>
-        get() = locationStates.notNullLatLng
-            .filter { state.events.data.isEmpty() } // TODO: this won't work with refreshing with SwipeRefreshLayout
-            .flatMapLatest { latLng -> loadingEventsUpdates(latLng) }
-
-    private val Flow<NearbyIntent.LoadMoreResults>.loadMoreResultsUpdates: Flow<NearbyStateUpdate>
-        get() = filterNot {
-            val events = state.events
-            events.status is Loading || !events.canLoadMore || events.data.isEmpty()
-        }.flatMapFirst {
-            locationStateProvider.locationStates.notNullLatLng.take(1)
-        }.flatMapFirst { latLng -> loadingEventsUpdates(latLng) }
-
-    private val Flow<LocationState>.notNullLatLng get() = map { it.latLng }.filterNotNull()
-
-    private fun loadingEventsUpdates(latLng: LatLng): Flow<NearbyStateUpdate> = getPagedEventsFlow(
-        currentEvents = state.events,
-        toEvent = { selectable -> selectable.item }
-    ) { offset ->
-        getNearbyEvents(latLng.lat, latLng.lng, offset)
-    }.map { resource ->
-        NearbyStateUpdate.Events.Loaded(resource)
-    }.onStart<NearbyStateUpdate> { emit(NearbyStateUpdate.Events.Loading) }
-
-    private val Flow<NearbyIntent.AddToFavouritesClicked>.addToFavouritesUpdates: Flow<NearbyStateUpdate>
-        get() = map {
-            val selectedEvents = state.events.data.filter { it.selected }.map { it.item }
-            withContext(ioDispatcher) { saveEvents(selectedEvents) }
-            signal(NearbySignal.FavouritesSaved)
-            NearbyStateUpdate.Events.AddedToFavourites(
-                snackbarText = addedToFavouritesMessage(eventsCount = selectedEvents.size),
-                onSnackbarDismissed = { viewModelScope.launch { intent(NearbyIntent.HideSnackbar) } }
-            )
-        }
 }
