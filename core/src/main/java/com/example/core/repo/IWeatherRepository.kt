dd6462605e418c759bddf30f271bd92fe77de747
TRM <therealmerengue@gmail.com>
2020-07-25 23:29:27 +0200

    toResource + getForecastTimed repo method.

--- core/src/main/java/com/example/core/repo/IWeatherRepository.kt
+++ core/src/main/java/com/example/core/repo/IWeatherRepository.kt
@@ -5,4 +5,9 @@ import com.example.core.model.weather.Forecast
 
 interface IWeatherRepository {
     suspend fun getForecast(lat: Double, lon: Double): Resource<Forecast>
+    suspend fun getForecastTimed(
+        lat: Double,
+        lon: Double,
+        secondsSinceEpoch: Long
+    ): Resource<Forecast>
 }
--- repo/src/main/java/com/example/repo/WeatherRepository.kt
+++ repo/src/main/java/com/example/repo/WeatherRepository.kt
@@ -3,16 +3,22 @@ package com.example.repo
 import com.example.core.model.Resource
 import com.example.core.model.weather.Forecast
 import com.example.core.repo.IWeatherRepository
+import com.example.core.util.ext.toResource
 import com.example.weatherapi.model.DarkSkyApi
-import com.haroldadmin.cnradapter.NetworkResponse
 
 class WeatherRepository(private val api: DarkSkyApi) : IWeatherRepository {
-    override suspend fun getForecast(
-        lat: Double, lon: Double
-    ): Resource<Forecast> = when (val response = api.getForecastAsync(lat, lon).await()) {
-        is NetworkResponse.Success -> Resource.Success(response.body)
-        is NetworkResponse.ServerError -> Resource.Error(response.body)
-        is NetworkResponse.NetworkError -> Resource.Error(response.error)
-        else -> throw IllegalArgumentException()
-    }
+
+    override suspend fun getForecast(lat: Double, lon: Double): Resource<Forecast> = api
+        .getForecastAsync(lat, lon)
+        .await()
+        .toResource()
+
+    override suspend fun getForecastTimed(
+        lat: Double,
+        lon: Double,
+        secondsSinceEpoch: Long
+    ): Resource<Forecast> = api
+        .getForecastTimedAsync(lat, lon, secondsSinceEpoch)
+        .await()
+        .toResource()
 }
